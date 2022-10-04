99e3f1e4e0b7f24a7e0129ac31728da359ff58bf
Sithlordbrock <brock.ford2002@gmail.com>
2022-10-03 22:32:01 -0600

    validated user input

--- _main_.py
+++ _main_.py
@@ -14,14 +14,23 @@ def main():
     
     currCard = cardDeck.drawCard()
     while player1.is_playing == True: 
-        print(f"{player1.name} current card is the {currCard.number} of {currCard.suit}")        #add current HP?
-        question = input("Do you think the next one is Higher or Lower? (h/l): ")
-        #validate that input
+        print(f"{player1.name} current card is the {currCard.number} of {currCard.suit}")
         nextCard = cardDeck.drawCard()
-        if question == "h":
-            result = currCard.cardCompareHigher(nextCard)
-        else:
-            result = currCard.cardCompareLower(nextCard)
+        validated = False
+        while(not validated):
+            question = input("Do you think the next one is Higher or Lower? (h/l): ") 
+            if question == "h":
+                validated = True
+                result = currCard.cardCompareHigher(nextCard)
+            elif question == "l":
+                validated = True
+                result = currCard.cardCompareLower(nextCard)
+            elif question == "quit":
+                validated = True
+            else:
+                print("If you meant to exit program, please enter \"quit\", otherwise try again... ")
+        if question == "quit":
+            break
         print(f"The next card is the {nextCard.number} of {nextCard.suit}...")
         player1.changePlayerScore(result)
         player1.checkPlayerPoints()
